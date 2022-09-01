a964ebea03ed8520f7caaab7a97f446c8a06ea33
= <=>
2022-09-01 11:46:57 -0400

    led_toggle added to main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print('hello world')
+from machine import Pin, Timer
+
+
+def tick(timer):
+    global led
+    led.toggle()
+
+
+led = Pin(25, Pin.OUT)
+tim = Timer()
+
+tim.init(freq=1, mode=Timer.PERIODIC, callback=tick)
