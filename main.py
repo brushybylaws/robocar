05efa3ec7288bf18e4a6a486de8fd5036d4a9df3
= <=>
2022-09-01 21:39:15 -0400

    added 2nd LED

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from machine import Pin, Timer, ADC
 import utime
+import rp2
+
 
 
 def tick(timer):
@@ -10,11 +12,13 @@ def tick(timer):
 led = Pin(25, Pin.OUT)
 tim = Timer()
 
-tim.init(freq=.5, mode=Timer.PERIODIC, callback=tick)
+tim.init(freq=.3, mode=Timer.PERIODIC, callback=tick)
 
 
 sensor_temp = ADC(4)
 conversion_factor = 3.3 / (2 ** 16 - 1)
+
+eled = Pin(15, Pin.OUT)
 
 while True:
     reading = sensor_temp.read_u16() * conversion_factor
@@ -25,4 +29,7 @@ while True:
     c_temp = 27 - (reading - 0.706)/0.001721
     f_temp = c_temp * 9/5 + 32
     print(f'{c_temp}C, {f_temp}F')
+    eled.value(True)
+    utime.sleep(2)
+    eled.value(False)
     utime.sleep(2)
